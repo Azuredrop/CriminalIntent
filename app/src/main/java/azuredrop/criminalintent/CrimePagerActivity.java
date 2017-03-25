3874308632b6aea51878e3e137d7d108f1e7a6ad
AzureDrop <taozhanbiao@gmail.com>
2017-03-25 10:08:09 +0800

    20170325_02 Chapter11

---
+++ app/src/main/java/azuredrop/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,71 @@
+package azuredrop.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+/**
+ * Created by Administrator on 2017/3/25.
+ */
+
+public class CrimePagerActivity extends FragmentActivity {
+    private ViewPager mViewPager;
+    private ArrayList<Crime> mCrimes;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mViewPager = new ViewPager(this);
+        mViewPager.setId(R.id.viewPager);
+        setContentView(mViewPager);
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+
+        FragmentManager fm = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
+            @Override
+            public Fragment getItem(int position) {
+                Crime c = mCrimes.get(position);
+                return CrimeFragment.newInstance(c.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                Crime c = mCrimes.get(position);
+                setTitle(c.getTitle());
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        UUID crimeId = (UUID)getIntent().getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+        for(int i = 0;i<mCrimes.size();i++) {
+            if (mCrimes.get(i).getId().equals(crimeId)) {
+                mViewPager.setCurrentItem(i);
+                break;
+            }
+        }
+    }
+}
